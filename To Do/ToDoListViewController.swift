c5d25edef4ca0bc572e9e7bf2121d25f0cdfc766
Raymond Tsang <RaymondTsang@raymonds-macbook-pro.local>
2018-09-21 14:54:48 -0700

    use UserDefaults as persistent storage for to-do list

--- To Do/ToDoListViewController.swift
+++ To Do/ToDoListViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var exampleTasks = ["groceries", "bike", "flu shot"]
+    let defaults = UserDefaults.standard
+    
+    var tasks = ["groceries", "bike", "flu shot"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let exampleTasks = defaults.array(forKey: "ToDoList") as? [String] {
+            tasks = exampleTasks
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,19 +33,18 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
-        cell.textLabel?.text = exampleTasks[indexPath.row]
+        cell.textLabel?.text = tasks[indexPath.row]
         
         return cell
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return exampleTasks.count
+        return tasks.count
     }
     
     //MARK - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(exampleTasks[indexPath.row])
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -58,7 +63,8 @@ class ToDoListViewController: UITableViewController {
         
         let alert = UIAlertController(title: "Add a new task to do!", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.exampleTasks.append(textField.text!)
+            self.tasks.append(textField.text!)
+            self.defaults.set(self.tasks, forKey: "ToDoList")
             self.tableView.reloadData()
         }
         
